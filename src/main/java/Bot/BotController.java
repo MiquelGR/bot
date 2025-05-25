fa914660c70aad02d877ce90bb54155c94d77959
Jordi Florit <jordifloritensenyat03@gmail.com>
2025-05-25 19:08:03 +0200

    bot service

--- src/main/java/Bot/BotController.java
+++ src/main/java/Bot/BotController.java
@@ -1,4 +1,20 @@
 package Bot;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bot")
+@RequiredArgsConstructor
 public class BotController {
+
+    private final BotService botService;
+
+    @PostMapping("/chat")
+    public ResponseEntity<String> chat(@RequestBody String prompt) {
+        String response = botService.getResponse(prompt);
+        return ResponseEntity.ok(response);
+    }
 }
+
--- src/main/java/Bot/BotService.java
+++ src/main/java/Bot/BotService.java
@@ -1,4 +1,22 @@
 package Bot;
 
+import dev.langchain4j.model.ollama.OllamaChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BotService {
+
+    private final OllamaChatModel model;
+
+    public BotService() {
+        this.model = OllamaChatModel.builder()
+                .baseUrl("http://localhost:11434")  // O `http://ollama:11434` si usas Docker
+                .modelName("templanza")             // Nombre del modelo cargado con Modelfile
+                .build();
+    }
+
+    public String getResponse(String prompt) {
+        return model.generate(prompt);
+    }
 }
+
